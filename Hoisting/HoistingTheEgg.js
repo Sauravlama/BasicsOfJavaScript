83e1b38c01c0f4d9f1aa78efb3c0ebe9af1b8278
Saurav Lama <Sauravlama231197@gmail.com>
2021-01-08 18:33:14 +0530

    First Question

---
+++ Hoisting/HoistingTheEgg.js
@@ -0,0 +1,8 @@
+//Welcome to the code of Hoisting the Egg
+
+//So i am going to write some code and you have to guess what the code actually does
+a=2;
+
+var a;
+
+console.log(a)
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # BasicsOfJavaScript
+
+<h3>
+Hey ! In this repo we are going to discuss some basics and cracy things that JavaScript is famous for</h3>
